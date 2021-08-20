e027c0b0d9a3417a5a2a05ef9dda7c22425fdee2
eddiecg <ecg@well.ox.ac.uk>
2021-08-20 11:01:16 +0100

    Adding verbose messages

--- R/stratifyPatients.R
+++ R/stratifyPatients.R
@@ -1,4 +1,4 @@
-stratifyPatients <- function(dat, k=50){
+stratifyPatients <- function(dat, k=50, verbose=T){
 
   # Verifying that predictors are present
   if( sum(!colnames(SepstratifieR::reference_set) %in% colnames(dat)) > 0 ) {
@@ -10,19 +10,37 @@ stratifyPatients <- function(dat, k=50){
   dat <- dat[,colnames(SepstratifieR::reference_set)]
 
   # Aligning data to the reference set
+  if(verbose) {
+    cat("\nAligning data to the reference set...")
+    cat("\nNumber of nearest neighours set to k=", k, sep="")
+  }
+
   merged_set <- data.frame(rbind(dat, SepstratifieR::reference_set))
   alignment_batch <- c(rep(2,nrow(dat)),
                        rep(1,nrow(SepstratifieR::reference_set)))
   mnn_res <- batchelor::mnnCorrect(t(merged_set), batch = alignment_batch, merge.order = list(1,2), k=k)
+
   aligned_set <- data.frame(t(assay(mnn_res)))
   aligned_dat <- aligned_set[1:nrow(dat),]
 
   # Identifying potential outliers
+  if(verbose) {
+    cat("\nIdentifying potential outlier samples...")
+  }
+
   outliers <- !(1:nrow(dat) %in% mnn_res@metadata$merge.info$pairs[[1]]$right)
 
   # Predicting labels
+  if(verbose) {
+    cat("\nStratifying samples into sepsis response signature (SRS) groups...")
+  }
   preds <- stats::predict(SepstratifieR::SRS_model, aligned_dat, type="raw")
   pred_probs <- stats::predict(SepstratifieR::SRS_model, aligned_dat, type="prob")
+
+  if(verbose) {
+    cat("\nAssigning samples a qunatitative sepsis response signature score (SRSq)...")
+  }
+
   preds_TRS <- stats::predict(SepstratifieR::SRSq_model, aligned_dat)
 
   res <- list(
@@ -34,6 +52,10 @@ stratifyPatients <- function(dat, k=50){
     SRSq_predictions = preds_TRS,
     is_outlier = outliers
   )
+
+  if(verbose) {
+    cat("\n... done!\n")
+  }
 
   return(res)
 
